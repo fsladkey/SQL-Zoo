2a200f051ff005d108697ed0f40f30c19216e455
Fred Sladkey <fsladkey@gmail.com>
2015-10-12 20:35:54 -0400

    Finished using null.

--- lib/04a_countries_bonus.rb
+++ lib/04a_countries_bonus.rb
@@ -17,6 +17,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+        MAX(countries.gdp)
+      FROM
+        countries
+      WHERE
+        continent = 'Europe'
+      )
   SQL
 end
 
@@ -24,6 +37,22 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      c1.continent,
+      c1.name,
+      c1.area
+    FROM
+      countries AS c1
+    WHERE
+      c1.area = (
+        SELECT
+          MAX(c2.area)
+        FROM
+          countries c2
+        WHERE
+          c1.continent = c2.continent
+      )
+
   SQL
 end
 
